aa3bfa0d8e96e72c7b12c382f4cd6f9f29a25e12
Veronica Bustabad <vbustabad@gmail.com>
2018-10-16 03:38:10 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -3,18 +3,18 @@ import React from 'react';
 class DigitalClicker extends React.Component {
   constructor() {
     super();
-    
+
      this.state = {
       timesClicked: 0,
     };
   }
-  
+
    handleClick = () => {
     this.setState({
       timesClicked: this.props.timesClicked + 1
     }, () => console.log(this.state.timesClicked))
   }
-  
+
    render() {
     return (
       <div>
